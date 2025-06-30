216cb0ae5fc27682c2ec20ea60cd1c5709997392
marc-berg <99531337+Marc-Berg@users.noreply.github.com>
2025-06-30 08:03:05 +0200

    New node "getHistory"

--- iob-config.js
+++ iob-config.js
@@ -23,14 +23,13 @@ function setupStaticResources(RED) {
         }
 
         RED.httpAdmin.use('/iobroker/shared', express.static(sharedPath, {
-            maxAge: 0, // No caching for TreeView JavaScript
+            maxAge: 0,
             etag: false,
             lastModified: false,
             setHeaders: (res, filePath) => {
                 if (filePath.endsWith('.js')) {
                     res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
                     res.setHeader('X-Content-Type-Options', 'nosniff');
-                    // Aggressive no-cache headers for JavaScript files
                     res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
                     res.setHeader('Pragma', 'no-cache');
                     res.setHeader('Expires', '0');
@@ -95,27 +94,69 @@ function setupAPIEndpoints(RED) {
                 const serverId = decodeURIComponent(req.params.serverId);
                 const connectionManager = require('./lib/manager/websocket-manager');
 
-                // Get objects to find adapters
-                const objects = await connectionManager.getObjects(serverId, 'system.adapter.*');
+                // Use the same method as TreeView - get all states
+                const states = await connectionManager.getStates(serverId);
                 const historyAdapters = [];
 
-                if (Array.isArray(objects)) {
-                    objects.forEach(obj => {
-                        if (obj && obj._id && obj.common) {
-                            const match = obj._id.match(/^system\.adapter\.(history|sql|influxdb)\.(\d+)$/);
-                            if (match) {
-                                const adapterName = `${match[1]}.${match[2]}`;
-                                historyAdapters.push({
+                if (states && typeof states === 'object') {
+                    const adapterConfigs = new Map();
+                    
+                    // Extract history adapter information from states
+                    Object.keys(states).forEach(stateId => {
+                        // Look for alive states of history adapters
+                        const aliveMatch = stateId.match(/^system\.adapter\.(history|sql|influxdb)\.(\d+)\.alive$/);
+                        if (aliveMatch) {
+                            const adapterType = aliveMatch[1];
+                            const instance = parseInt(aliveMatch[2]);
+                            const adapterName = `${adapterType}.${instance}`;
+                            
+                            const state = states[stateId];
+                            const isAlive = state && typeof state.val === 'boolean' ? state.val : Boolean(state?.val);
+                            
+                            if (!adapterConfigs.has(adapterName)) {
+                                adapterConfigs.set(adapterName, {
                                     name: adapterName,
-                                    type: match[1],
-                                    instance: parseInt(match[2]),
-                                    enabled: obj.common.enabled || false,
-                                    alive: obj.common.alive || false,
-                                    title: obj.common.title || obj.common.name
+                                    type: adapterType,
+                                    instance: instance,
+                                    enabled: false, // Will be updated if enabled state found
+                                    alive: isAlive,
+                                    title: adapterName
                                 });
+                            } else {
+                                adapterConfigs.get(adapterName).alive = isAlive;
+                            }
+                        }
+                        
+                        // Look for enabled states of history adapters
+                        const enabledMatch = stateId.match(/^system\.adapter\.(history|sql|influxdb)\.(\d+)\.enabled$/);
+                        if (enabledMatch) {
+                            const adapterType = enabledMatch[1];
+                            const instance = parseInt(enabledMatch[2]);
+                            const adapterName = `${adapterType}.${instance}`;
+                            
+                            const state = states[stateId];
+                            const isEnabled = state && typeof state.val === 'boolean' ? state.val : Boolean(state?.val);
+                            
+                            if (!adapterConfigs.has(adapterName)) {
+                                adapterConfigs.set(adapterName, {
+                                    name: adapterName,
+                                    type: adapterType,
+                                    instance: instance,
+                                    enabled: isEnabled,
+                                    alive: false, // Will be updated if alive state found
+                                    title: adapterName
+                                });
+                            } else {
+                                adapterConfigs.get(adapterName).enabled = isEnabled;
                             }
                         }
                     });
+
+                    // Convert map to array
+                    for (const config of adapterConfigs.values()) {
+                        historyAdapters.push(config);
+                        console.log(`[ioBroker] Found adapter ${config.name}: enabled=${config.enabled}, alive=${config.alive}`);
+                    }
                 }
 
                 // Sort by type and instance
